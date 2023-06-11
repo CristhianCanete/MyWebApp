c6d80af548ac5bca09db9396b7944fc1e1ed0c33
CristhianCanete <canetecristhian@gmail.com>
2023-06-11 10:09:01 -0400

    se agrego las vistas del login-logout

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
